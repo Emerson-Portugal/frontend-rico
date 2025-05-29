054f597c9f77054589beb6c73c6edf267216aca1
emersoncrp <emersoncrp@gmail.com>
2025-05-29 11:23:42 -0500

    update:environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://127.0.0.1:8000/api',
+  apiBaseUrl: 'http://24.199.85.129:8000/api',
   // apiBaseUrl: 'https://localhost:7146/api',
 }
