688bf1219b9f3e5c4875acbec0cb72e08b8ed853
emersoncrp <emersoncrp@gmail.com>
2025-07-01 20:48:15 -0500

    update dash

--- src/app/shared/pages/config/dash-board-page/dash-board-page.component.ts
+++ src/app/shared/pages/config/dash-board-page/dash-board-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core'
+import { WidgetQuickValueCenterComponent } from '@shared/third-party/layouts/components/widget-quick-value-center/widget-quick-value-center.component'
+
 
 @Component({
   selector: 'app-dash-board-page',
   standalone: true,
-  imports: [],
+  imports: [WidgetQuickValueCenterComponent],
   templateUrl: './dash-board-page.component.html',
   styleUrl: './dash-board-page.component.scss',
 })
